d775cdb8f19b66b535bfcad067785e0c07d8c2e8
tarikul-islam <tariikul12@gmail.com>
2023-10-27 22:13:25 +0600

    dshoaboard working

---
+++ src/Layout/DashboardLayout.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import Navbar from '../Components/Navbar/Navbar';
+import { Outlet } from 'react-router-dom';
+import Footer from '../Components/Footer/Footer';
+
+const DashboardLayout = () => {
+    return (
+        <div>
+           <Navbar></Navbar> 
+           <Outlet></Outlet>
+           <Footer></Footer>
+        </div>
+    );
+};
+
+export default DashboardLayout;
--- src/Provider/router.jsx
+++ src/Provider/router.jsx
@@ -5,6 +5,8 @@ import {
 import Main from "../Layout/Main";
 import Error from "../Components/Error/Error";
 import Home from "../Pages/Home/Home/Home";
+import DashboardLayout from "../Layout/DashboardLayout";
+import Dashboard from "../Pages/Dashboard/Dashboard/Dashboard";
 
 export const router = createBrowserRouter([
     {
@@ -15,6 +17,17 @@ export const router = createBrowserRouter([
         {
           path:"/",
           element:<Home></Home>
+        }
+      ]
+    },
+    {
+      path: "/dashboard",
+      element: <DashboardLayout></DashboardLayout>,
+      errorElement:<Error></Error>,
+      children:[
+        {
+          path:"/dashboard",
+          element:<Dashboard></Dashboard>
         }
       ]
     },

df2e49488b4933d6e11221be3c45d6470e277264
tarikul-islam <tariikul12@gmail.com>
2023-11-02 06:12:08 +0600

    luxury section design done

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -2,20 +2,19 @@
 import Navbar from "../Components/Navbar/Navbar";
 import Footer from "../Components/Footer/Footer";
 
-import { motion, useScroll } from "framer-motion"
 import { Outlet } from "react-router-dom";
 
 const Main = () => {
-    const { scrollYProgress } = useScroll();
+   
     return (
         <>
-        <motion.div  className="overflow-hidden" >
+        <div  className="overflow-hidden" >
             <Navbar></Navbar>
  <Outlet></Outlet>
             <Footer></Footer>
 
             
-        </motion.div>
+        </div>
         </>
     );
 };

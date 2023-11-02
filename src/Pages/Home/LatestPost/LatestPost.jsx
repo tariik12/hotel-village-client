a82318912cc880e2e26e3df36351c3d4cf719cca
tarikul-islam <tariikul12@gmail.com>
2023-11-02 08:23:00 +0600

    latest post design almost done

--- src/Pages/Home/LatestPost/LatestPost.jsx
+++ src/Pages/Home/LatestPost/LatestPost.jsx
@@ -57,26 +57,52 @@ const roomBooks =[
 import AOS from 'aos';
 import 'aos/dist/aos.css'; 
 import { useEffect } from 'react';
-import { useGetLatestProductsQuery } from '../../../redux/api/hotelVillageApi';
+import { useGetHotelServicesDataQuery } from '../../../redux/api/hotelVillageApi';
+import { FaWifi } from 'react-icons/fa';
+import {GiForkKnifeSpoon, GiBarbedNails } from 'react-icons/gi';
 
 const LatestPost = () => {
-    const {data:LatestPosts, isLoading} = useGetLatestProductsQuery()
+    const {data:LatestPosts } = useGetHotelServicesDataQuery()
     useEffect(() => {
         AOS.init();
       }, [])
    return (
-    <div className="md:flex justify-around mt-10" >
-       <div>
-       {
-        LatestPosts?.map(({ image, Category, description, shorting }) => 
-            <div  data-aos={shorting % 2 === 1 ? 'fade-left' : 'fade-right'} className={`md:flex w-full justify-between mt-3 ${shorting % 2 === 1 ? 'md:flex-row-reverse' : ''}`} key={image}>
-                <img className="md:w-[300px] h-[300px]" src={image} alt="" />
-                <p>{Category}</p>
-                <p className="">{description}</p>
+    <div className="md:flex justify-around mt-10 mx-4 ">
+    <div>
+      {LatestPosts
+        ?.slice() // Create a shallow copy of the array to avoid mutating the original
+        .reverse() // Reverse the order of the copied array
+        .map(({ _id,image, name, description,  }, reversedIndex) => (
+          <div
+            data-aos={reversedIndex % 2 === 0 ? 'fade-left' : 'fade-right'}
+            className={`md:flex bg-gradient-to-t from-[#f1f1f3ef] via-[#228eac3d] to-[#f7f4f483] hover:bg-gradient-to-t hover:from-[#f1f1f3cf] hover:via-[#228eac97] hover:to-[#f7f4f4b8] w-full justify-between items-center mt-3 ${
+              reversedIndex % 2 === 0 ? 'md:flex-row-reverse' : ''
+            }`}
+            key={_id }
+          >
+            <img className="md:w-[600px] h-[300px]" src={image} alt="" />
+            <div className='text-center' data-aos='zoom-in'>
+            <h3 className="text-center text-2xl text-orange-600">{name}</h3>
+            <div className='flex justify-center gap-4 mt-3'>
+           < p className='bg-orange-500 p-2 rounded-xl shadow-md'>max. guests :
+2 adults
+bed </p>
+<p className='bg-orange-500 p-2 rounded-xl shadow-md'>type : 2 double or 1 king</p>
             </div>
-         )}
-       </div>
+<div className='flex justify-center gap-6 mt-3 text-2xl'>
+    <FaWifi/>
+    <GiForkKnifeSpoon/>
+    <GiBarbedNails/>
+</div>
+            <p className="px-5 mt-5 w-3/4 mx-auto"  >{description} </p>
+            <div><button className='bg-slate-700 py-2 px-4 rounded-3xl text-white text-xs'> book Now</button></div>
+            </div>
+          </div>
+        ))}
     </div>
+      
+  </div>
+  
 )
 };
 
